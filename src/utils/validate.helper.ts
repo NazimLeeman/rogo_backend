f885d8dcc6f3fcd57cd788e1fa7930e23fbd2c03
nazimleeman <nazimleeman@gmail.com>
2024-05-24 00:19:31 +0600

    env example added

--- src/utils/validate.helper.ts
+++ src/utils/validate.helper.ts
@@ -11,16 +11,11 @@ export const validateLogin = (
   
   export const validateRegsitration = (
     data: any
-  ): data is { email: string; password: string, name: string, dob: Date | string, gender: 'male' | 'female' | 'others' } => {
+  ): data is { email: string; password: string, name: string } => {
     return (
       data.email &&
       data.password &&
-      data.name &&
-      data.dob &&
-      data.gender &&
       typeof data.name === "string" &&
-      (data.dob instanceof Date || typeof data.dob === 'string') &&
-      (data.gender === "male" || data.gender === "female" || data.gender === "others") &&
       typeof data.email === "string" &&
       typeof data.password === "string"
     );
